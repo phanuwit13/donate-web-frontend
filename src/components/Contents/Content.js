559c2aa63cf6ac6c4a78ad5c2f890d4162344865
phanuwit13 <big.phanuwit@gmail.com>
2021-03-11 12:35:11 +0700

    test api

--- src/components/Contents/Content.js
+++ src/components/Contents/Content.js
@@ -13,40 +13,7 @@ class Content extends Component {
           <div className="box-head-title" >
             <span className="head-title">โครงการทั้งหมด</span>
           </div>
-          <div className="row">
-            {this.props.data?.map((value, index) => {
-              return (
-                <div key={index} className="col-12 col-sm-12 col-lg-6 col-xl-4">
-                  <div className="card">
-                    <img
-                      src={
-                        "https://service-donate.herokuapp.com/images/project-img/" + value.img
-                      }
-                      className="card-img-top img-content rounded-bottom"
-                      alt=""
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">{value.title}</h5>
-                      <p className="card-text">{value.detail}</p>
-                      {/* <div className='card-footer bg-transparent border-success'>Footer</div> */}
-                    </div>
-                    <div className="footer-card nav justify-content-center">
-                      <a
-                        type="button"
-                        className="btn btn-light donate-btn"
-                        onClick={() => {
-                          this.toPath("form/"+index);
-                          this.props.action(value)
-                        }}
-                      >
-                        ร่วมบริจาค
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          
         </div>
       </Fragment>
     );
